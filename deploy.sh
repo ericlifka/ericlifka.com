6f772bf1eac64ebcca63baea29bbed83ffe6ebf1
Eric Lifka <eric.lifka@gmail.com>
2015-03-23 03:08:52 -0400

    simple hacky deploy script to move builds into ericlifka.github.io repo

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+rsync -r dist/ ../ericlifka.github.io
+cd ../ericlifka.github.io
+git add --all
+git commit -m "build from https://github.com/ericlifka/ericlifka.com"
+git push
+cd ../ericlifka.com
