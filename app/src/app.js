3eafc2f8092e6d9d94876d5bd23929e8f60ba3a9
Eric Lifka <eric.lifka@gmail.com>
2015-03-25 22:33:30 -0400

    hook into scroll event to check scroll position

--- app/src/app.js
+++ app/src/app.js
@@ -1,1 +1,3 @@
-console.log('test');
+import dynamicHeader from './dynamic-header';
+
+dynamicHeader();
---
+++ app/src/dynamic-header.js
@@ -0,0 +1,8 @@
+export default function () {
+    const titleBar = document.querySelector(`.title-card`);
+
+
+    window.addEventListener('scroll', function () {
+        console.log(window.scrollY);
+    });
+};
