e39b3e78986c7451dbbdf055ffa14cfa5a56b07b
Eric Lifka <eric.lifka@gmail.com>
2015-03-25 22:45:58 -0400

    set header subclass based on page scroll position

--- app/src/dynamic-header.js
+++ app/src/dynamic-header.js
@@ -1,8 +1,38 @@
 export default function () {
-    const titleBar = document.querySelector(`.title-card`);
-
+    const titleBar = document.querySelector('.title-card');
+    let titleBarState = 'full';
 
     window.addEventListener('scroll', function () {
-        console.log(window.scrollY);
+        const scrollOffset = window.scrollY;
+
+        if (titleBarState === 'full') {
+            if (scrollOffset > 40) {
+                titleBarState = 'collapsing';
+                titleBar.classList.remove('full');
+                titleBar.classList.add('collapsing');
+            }
+        }
+
+        else if (titleBarState === 'collapsing') {
+            if (scrollOffset <= 40) {
+                titleBarState = 'full';
+                titleBar.classList.remove('collapsing');
+                titleBar.classList.add('full');
+            }
+
+            if (scrollOffset > 230) {
+                titleBarState = 'collapsed';
+                titleBar.classList.remove('collapsing');
+                titleBar.classList.add('collapsed');
+            }
+        }
+
+        else if (titleBarState === 'collapsed') {
+            if (scrollOffset <= 230) {
+                titleBarState = 'collapsing';
+                titleBar.classList.remove('collapsed');
+                titleBar.classList.add('collapsing');
+            }
+        }
     });
 };
